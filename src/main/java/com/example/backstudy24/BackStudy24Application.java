fd78e60ef5b060adb459eaf869c7ea18cd72ceed
kim jihyeon <jihyeon546@gmail.com>
2024-09-23 16:50:03 +0900

    initial commit

---
+++ src/main/java/com/example/backstudy24/BackStudy24Application.java
@@ -0,0 +1,13 @@
+package com.example.backstudy24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackStudy24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackStudy24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backstudy24/BackStudy24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backstudy24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackStudy24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
