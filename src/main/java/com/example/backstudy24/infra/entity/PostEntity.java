2fb0587e008ec3a2e637c2941cec8fdd854a8d18
kim jihyeon <jihyeon546@gmail.com>
2024-09-23 18:51:16 +0900

    1주차

--- src/main/java/com/example/backstudy24/infra/entity/PostEntity.java
+++ src/main/java/com/example/backstudy24/infra/entity/PostEntity.java
@@ -2,7 +2,6 @@ package com.example.backstudy24.infra.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
